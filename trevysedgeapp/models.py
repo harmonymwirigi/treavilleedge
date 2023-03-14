04cec61abab7980926ad0db0f43f2b8bedbd1c3a
ngugipiano <rickmuret@gmail.com>
2023-03-14 12:57:33 +0300

    14 March 23, py files removed and dashboard remaining

--- trevysedgeapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Feature(models.Model):
-    name=models.CharField(max_length=100)
-    
-    details=models.CharField(max_length=500)
-
-    
